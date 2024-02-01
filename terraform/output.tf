b254433f988c2102434b6f8020905a763498a284
Jeremieon <jerry_slade@outlook.com>
2024-02-02 00:34:21 +0100

    fixed aws instance name error

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,6 +1,6 @@
 output "public_ip" {
-  value = aws_instance.ec2_instance.public_ip
+  value = aws_instance.myEc2-instance.public_ip
 }
 output "id" {
-  value = aws_instance.id
+  value = aws_instance.myEc2-instance.id
 }
